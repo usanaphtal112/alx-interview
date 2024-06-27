1d2097d434c4e92b717169154348a08c901b19cc
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2024-06-26 23:27:27 -0700

    Added function doc string

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,10 +8,11 @@ def pascal_triangle(n):
 
     if n <= 0:
         return []
-    triangle = [[1]]
-    for i in range(1, n):
-        row = [1] + [triangle[i - 1][j - 1] + triangle[i - 1][j] for j in range(1, i)] + [1]
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
         triangle.append(row)
-
     return triangle
 
